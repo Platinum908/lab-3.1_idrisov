c5cac4aeb2929d37c6ed3774b7dac7470943c9eb
Administrator <Administrator@WIN-QH3366O2A51>
2023-11-18 00:10:48 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,18 @@
+﻿//3.1 Средний уровень #15 
+try
+{
+    Console.Write("Введите k:");
+    double k = double.Parse(Console.ReadLine());
+    double S = 0;
+    for (int n = 1; n <= k; n++)
+    {
+        double factorial = 1;
+        for (int i = 1; i <= n; i++)
+        {
+            factorial *= i;
+        }
+        S += (Math.Pow(-1, 2 * n) * Math.Pow((Math.Pow(n, 2) - 9), 2)) / 3 * factorial;
+    }
+    Console.WriteLine($"S={S:F2}");
+}
+catch { Console.WriteLine("Ошибка"); }
